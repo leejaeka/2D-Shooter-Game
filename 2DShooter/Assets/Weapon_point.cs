4100e149b2a454d4ccfe092a7c8e58666a4a2068
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-05-14 03:19:43 -0700

    weapon added with background now follows the cursor
    yes

---
+++ 2DShooter/Assets/Weapon_point.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_point : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle - 80, Vector3.forward);
+            transform.rotation = rotation;
+    }
+
+}
