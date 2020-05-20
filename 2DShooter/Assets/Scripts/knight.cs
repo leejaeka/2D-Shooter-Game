02b4ba4841cb0b203d77462f72e6c36f639c6032
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-05-20 01:13:10 -0700

    UIUIUI
    YES

--- 2DShooter/Assets/Scripts/knight.cs
+++ 2DShooter/Assets/Scripts/knight.cs
@@ -10,12 +10,17 @@ public class knight : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveAmount;
     private Animator anim;
+
+    public List<GameObject> hearts;
+    public GameObject fullHeart;
+    public UnityEngine.UI.GridLayoutGroup gridLayoutGroup;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        UpdateHealthUI(health);
     }
 
     // Update is called once per frame
@@ -32,14 +37,29 @@ public class knight : MonoBehaviour
             anim.SetBool("isRunning", false);
         }
     }
+    private void UpdateHealthUI(int currentHealth)
+    {
+        hearts.Clear();
+        GameObject[] GOS = GameObject.FindGameObjectsWithTag("HP");
+        foreach (GameObject item in GOS) {
+            Destroy(item);
+        }
+
+        for (int i = 0; i < currentHealth; i++)
+        {
+            GameObject heart = Instantiate(fullHeart, new Vector3(0, 0, 10), Quaternion.identity) as GameObject;
+            hearts.Add(heart);
+            hearts[i].transform.SetParent(gridLayoutGroup.transform);
+        }
+    }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
-
     }
     public void TakeDamage(int damageAmount)
     {
         health -= damageAmount;
+        UpdateHealthUI(health);
         if (health <= 0)
         {
             Destroy(this.gameObject);
